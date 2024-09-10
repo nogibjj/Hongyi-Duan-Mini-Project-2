231a0363e68d2f9e2388f3b0cdda1438ef89ad0e
Lulu-1121 <hd162@duke.edu>
2024-09-11 01:35:01 +0800

    update

--- test_main.py
+++ test_main.py
@@ -1,7 +1,21 @@
-from main import add
+from main import *
+import pandas as pd
 
-def test_add():
-    assert add(1, 2) == 3
+data = pd.read_excel("Project-Management-Sample-Data.xlsx")
+x = data['Days Required']
+
+
+def test_mean():
+    assert round(get_mean(x), 5) == round(x.mean(), 5)
+
+def test_median():
+    assert round(get_median(x), 5) == round(x.median(), 5)
+
+def test_std():
+    assert round(get_std(x), 5) == round(x.std(), 5)
 
 if __name__ == "__main__":
-    test_add()
+    test_mean()
+    test_median()
+    test_std()
+    get_plot(x)
